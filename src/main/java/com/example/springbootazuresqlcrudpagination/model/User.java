8198d81af18be52d603d545814f0e64f296845a3
Iryna Tiutiun <tiutiuniryna@gmail.com>
2023-06-08 16:26:39 +0200

    created model

---
+++ src/main/java/com/example/springbootazuresqlcrudpagination/model/User.java
@@ -0,0 +1,62 @@
+package com.example.springbootazuresqlcrudpagination.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "country")
+    private String country;
+    @Column(name = "email")
+    private String email;
+
+    public User(Long id, String name, String country, String email) {
+        super();
+        this.id = id;
+        this.name = name;
+        this.country = country;
+        this.email = email;
+    }
+
+    public User() {
+        super();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+}
