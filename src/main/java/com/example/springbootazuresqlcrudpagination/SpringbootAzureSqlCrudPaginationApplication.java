1aef189e4c49007acb11589e6a665d54fca7d3f7
Iryna Tiutiun <tiutiuniryna@gmail.com>
2023-06-08 16:15:32 +0200

    Initial project

---
+++ src/main/java/com/example/springbootazuresqlcrudpagination/SpringbootAzureSqlCrudPaginationApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootazuresqlcrudpagination;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAzureSqlCrudPaginationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootAzureSqlCrudPaginationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbootazuresqlcrudpagination/SpringbootAzureSqlCrudPaginationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootazuresqlcrudpagination;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAzureSqlCrudPaginationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
