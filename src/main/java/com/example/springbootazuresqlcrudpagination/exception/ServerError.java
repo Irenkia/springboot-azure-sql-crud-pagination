e3f0db21a6eaa553231bb96dc1a89310e4a46bab
Iryna Tiutiun <tiutiuniryna@gmail.com>
2023-06-08 16:54:23 +0200

    created exception

---
+++ src/main/java/com/example/springbootazuresqlcrudpagination/exception/ServerError.java
@@ -0,0 +1,9 @@
+package com.example.springbootazuresqlcrudpagination.exception;
+
+public class ServerError extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+
+    public ServerError(String message) {
+        super(message);
+    }
+}
